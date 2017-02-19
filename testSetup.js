4d336bf8999bbbd9621a748451c38dca8edd96ac
Alex Foster <alexjfno1@gmail.com>
2017-02-19 13:04:06 +0000

    Add a SearchBox component to enter search term

--- testSetup.js
+++ testSetup.js
@@ -1,5 +1,15 @@
 import { jsdom } from 'jsdom';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import chai from 'chai';
 
 global.document = jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
 global.navigator = global.window.navigator;
+global.shallow = shallow;
+global.expect = expect;
+global.sinon = sinon;
+
+chai.use(sinonChai);
